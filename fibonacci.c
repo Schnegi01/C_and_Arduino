9fe709e5b9f1867166e57b828e30fd7a3a986307
Shnegi01 <maximilian.haidinger.at@gmail.com>
2025-01-28 00:41:24 +0100

    led blinken + makefile

--- fibonacci.c
+++ fibonacci.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-  unsigned long long int number_1 = 0;
-  unsigned long long int number_2 = 1;
+  unsigned long long int number_1 = 0UL;
+  unsigned long long int number_2 = 1UL;
   for (int i = 0; i < 100; i++) {
     unsigned long long int number_3 = number_1 + number_2;
     printf("%d: %llu\n", i + 2, number_3);
